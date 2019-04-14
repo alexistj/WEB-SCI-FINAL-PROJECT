ac1f22633b419fe9511c4ee6076b5db425b94185
justinchen673 <justinchenchen673@gmail.com>
2019-04-14 14:38:27 -0400

    made the compiler take specific questions and pull from mongo

--- testingCompiler/myCtrl.js
+++ testingCompiler/myCtrl.js
@@ -1,18 +1,20 @@
 app.controller('myCtrl', ['$scope','$http', function($scope, $http) {
 
-    $http.get('http://localhost:8080/questions.json').then(function(data) {
+    $scope.topic = "compilerQuestions";
+    $scope.questionNum = 2;
+
+    $http.get('http://localhost:8080/retrieveQuestion/' + $scope.topic + "/" + $scope.questionNum).then(function(data) {
+
         // Initialize the question as well as the test inputs for this question
-        $scope.questionNumber = Math.floor((Math.random() * data.data.questions.length));
-        $scope.question = data.data.questions[$scope.questionNumber].description;
-        $scope.testInputs = data.data.questions[$scope.questionNumber].testInputs;
-        $scope.testOuputs = data.data.questions[$scope.questionNumber].testOuputs;
+        $scope.question = data.data[0].description;
+        $scope.testInputs = data.data[0].testInputs;
+        $scope.testOuputs = data.data[0].testOuputs;
 
         // Add starter code to the text area
         document.getElementById("code").innerHTML = "#include<iostream>\n#include <string>\n\nusing namespace std;\n\nint main() {\n";
         // This is for question specific code
-        document.getElementById("code").innerHTML += "    " + data.data.questions[$scope.questionNumber].inputType + " x;\n";
-        document.getElementById("code").innerHTML += "    cin >> x;\n\n    cout << x;\n    return 0;\n}";
-
+        document.getElementById("code").innerHTML += "    " + data.data[0].inputType + " x;\n";
+        document.getElementById("code").innerHTML += "    cin >> x;\n\n    return 0;\n}";
     });
 
     $scope.checkCode = function() {
@@ -21,7 +23,7 @@ app.controller('myCtrl', ['$scope','$http', function($scope, $http) {
         var editedText = document.getElementById("code").value.replace(/(\n)/gm, "\\n").replace(/(\")/gm, "\\\"");
         var text = '{ "code":"' + editedText + '" } ';
 
-        $http.post("http://localhost:8080/compilecode/" + $scope.questionNumber, text).then(function(data) {
+        $http.post("http://localhost:8080/compilecode/" + $scope.topic + "/" + $scope.questionNum, text).then(function(data) {
             var body = document.getElementsByTagName("center")[0];
             // Output each test with the outputs and expected outputs
             for (var i = 0; i < $scope.testOuputs.length; i++) {
--- testingCompiler/server.js
+++ testingCompiler/server.js
@@ -1,55 +1,117 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+const MongoClient = require("mongodb").MongoClient;
 var bodyParser = require('body-parser');
 var fs = require('fs');
-var jsdom = require('jsdom');
 app.use(bodyParser());
+
+// Mongo connection
+const CONNECTION_URL = "mongodb+srv://admin:adminpassword@cluster0-f0kkf.mongodb.net/test?retryWrites=true";
+const DATABASE_NAME = "questions";
 
 //compileX
 var compiler = require('compilex');
 var option = {stats : true};
 compiler.init(option);
 
-app.get('/' , function (req , res ) {
-	// Uses our made HTML file for the frontend
-	res.sendfile( __dirname + "/index.html");
-});
+MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+    if(error) {
+        throw error;
+    }
+
+    database = client.db(DATABASE_NAME);
+    console.log("Connected to `" + DATABASE_NAME + "`!");
+
+	/*
+	var myobj = [
+		{
+            "description": "Write a program that squares an input.",
+            "testInputs": ["-5", "0", "1", "10", "123"],
+            "testOuputs": ["25", "0", "1", "100", "15129"],
+            "inputType": "int",
+			"num": 0
+        },
+        {
+            "description": "Write a program that multiplies an input by 10.",
+            "testInputs": ["-3", "3", "9", "10", "0"],
+            "testOuputs": ["-30", "30", "90", "100", "0"],
+            "inputType": "int",
+			"num": 1
+        },
+        {
+            "description": "Write a program that adds 10 to an input.",
+            "testInputs": ["-5", "0", "1", "10", "123"],
+            "testOuputs": ["5", "10", "11", "20", "133"],
+            "inputType": "int",
+			"num": 2
+        },
+        {
+            "description": "Write a program that appends \"Hi \" to the beginning of the string and an exclamation point at the end.",
+            "testInputs": ["Justin", "Alexis", "Jenny", "Aaron"],
+            "testOuputs": ["Hi Justin!", "Hi Alexis!", "Hi Jenny!", "Hi Aaron!"],
+            "inputType": "string",
+			"num": 3
+        }
+	];
+
+	database.collection("compilerQuestions").insertMany(myobj, function(err, res) {
+    	if (err) throw err;
+    	console.log("Number of documents inserted: " + res.insertedCount);
+  	});
+	*/
+
+	app.get('/' , function (req , res ) {
+		// Uses our made HTML file for the frontend
+		res.sendfile( __dirname + "/index.html");
+	});
+
+	app.get('/retrieveQuestion/:topic/:questionNum', function(req,res) {
+		var count = parseInt(req.params.questionNum, 10);
+		var query = { num: count };
+		database.collection(req.params.topic).find(query).toArray(function(err, result) {
+            if (err) throw err;
+            res.send(result);
+        });
+    });
+
+	app.post('/compilecode/:topic/:questionNum' , function (req , res, next ) {
+
+		var count = parseInt(req.params.questionNum, 10);
+		var query = { num: count };
+		database.collection(req.params.topic).find(query).toArray(function(err, result) {
+            if (err) throw err;
+			var allResults = [];
+			// Reformat the code for processing
+			var code = req.body.code.replace(/(\\n)/gm, "\n");
 
 
-app.post('/compilecode/:questionNum' , function (req , res, next ) {
-	// Read the inputs directly from the JSON. The question number is in the URL
-	var obj = JSON.parse(fs.readFileSync('questions.json', 'utf8'));
-	var inputs = obj.questions[req.params.questionNum].testInputs;
-
-	var allResults = [];
-	// Reformat the code for processing
-	var code = req.body.code.replace(/(\\n)/gm, "\n");
-
-
-	// Begins the compilation. There will always be 5 inputs, and this is to
-	// maintain it being synchronous
-	var envData = { OS : "windows" , cmd : "g++", options: {timeout:5000 } };
-	compiler.compileCPPWithInput(envData , code , inputs[0] , function (data) {
-		// Each of these adds the data to the list of results to return
-		allResults.push(data);
-		compiler.compileCPPWithInput(envData , code , inputs[1] , function (data) {
-			allResults.push(data);
-			compiler.compileCPPWithInput(envData , code , inputs[2] , function (data) {
+			// Begins the compilation. There will always be 5 inputs, and this is to
+			// maintain it being synchronous
+			var envData = { OS : "windows" , cmd : "g++", options: {timeout:5000 } };
+			compiler.compileCPPWithInput(envData , code , result[0]["testInputs"][0] , function (data) {
+				// Each of these adds the data to the list of results to return
 				allResults.push(data);
-				compiler.compileCPPWithInput(envData , code , inputs[3] , function (data) {
+				compiler.compileCPPWithInput(envData , code , result[0]["testInputs"][1] , function (data) {
 					allResults.push(data);
-					compiler.compileCPPWithInput(envData , code , inputs[4] , function (data) {
+					compiler.compileCPPWithInput(envData , code , result[0]["testInputs"][2] , function (data) {
 						allResults.push(data);
+						compiler.compileCPPWithInput(envData , code , result[0]["testInputs"][3] , function (data) {
+							allResults.push(data);
+							compiler.compileCPPWithInput(envData , code , result[0]["testInputs"][4] , function (data) {
+								allResults.push(data);
 
-						// Sends the results to Angular
-						res.send(allResults);
+								// Sends the results to Angular
+								res.send(allResults);
+							});
+						});
 					});
 				});
 			});
-		});
+        });
 	});
 });
+
 
 // For getting all the local files
 app.get('/myApp.js', function(req, res) {
